1673d3f0623990c025ce4a05aaf3b05951acd4b7
lawson-ng <abraham.lawson.dev@gmail.com>
2023-11-28 08:17:24 +0700

    update title

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "lawson ng",
+    description: "welcome to lawson-ng!",
   },
 
   // CONFIG configration (required)
