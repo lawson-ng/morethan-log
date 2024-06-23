8e4ac0222516dabb60260e69377a81dba3b70298
lawson-ng <abraham.lawson.dev@gmail.com>
2024-06-24 00:41:05 +0700

    feat: comment

--- site.config.js
+++ site.config.js
@@ -54,9 +54,9 @@ const CONFIG = {
     },
   },
   utterances: {
-    enable: false,
+    enable: true,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "lawson-ng/blog-comments",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
